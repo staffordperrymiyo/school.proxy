2a90142c7b681316cd6ebd18ade487db88547b35
staffordperrymiyo <168000797+staffordperrymiyo@users.noreply.github.com>
2025-02-03 16:36:01 +0000

    Updated Proxy

--- server.js
+++ server.js
@@ -1,32 +1,51 @@
 const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const axios = require('axios'); 
 const app = express();
+const path = require("path")
 
-let targetUrl = 'https://search.brave.com';
+app.use(express.json()); 
 
-app.use((req, res, next) => {
-  const newTargetUrl = req.query.target;
-  if (newTargetUrl) {
-    targetUrl = newTargetUrl;
+app.get("/client.html", (req, res) => {
+  res.sendFile(path.resolve("client.html"))
+})
+
+app.post('/', async (req, res) => {
+  const { url } = req.body;
+
+  if (!url) {
+    return res.status(400).send('No URL provided.');
   }
-  next();
+
+  try {
+    const response = await axios.get(url);
+    let html = response.data;
+
+    html = html.replace(/src="(https?:\/\/[^"]+)"/g, 'src="/proxy?url=$1"');
+    html = html.replace(/href="(https?:\/\/[^"]+)"/g, 'href="/proxy?url=$1"');
+
+    res.send(html); 
+  } catch (error) {
+    res.status(500).send('Error fetching the HTML from the provided URL.');
+  }
 });
 
-app.use(
-  '/',
-  createProxyMiddleware({
-    target: targetUrl,
-    changeOrigin: true,
-    secure: false,
-    onProxyRes(proxyRes, req, res) {
-      
-    },
-    onProxyReq(proxyReq, req, res) {
-      
-    }
-  })
-);
+app.get('/proxy', async (req, res) => {
+  const { url } = req.query;
 
-app.listen(3000, () => {
-  console.log('Proxy server is running on http://localhost:3000');
+  if (!url) {
+    return res.status(400).send('No URL provided.');
+  }
+
+  try {
+
+    const response = await axios.get(url, { responseType: 'arraybuffer' });
+    res.set(response.headers);
+    res.send(response.data); 
+  } catch (error) {
+    res.status(500).send('Error fetching the content from the provided URL.');
+  }
+});
+
+app.listen(3004, () => {
+  console.log('Proxy server is running on http://localhost:3004');
 });
