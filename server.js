1cfbf87fa25f5cfd4ae031d4d38c9f8e5a5f9cca
staffordperrymiyo <168000797+staffordperrymiyo@users.noreply.github.com>
2025-02-03 12:37:07 +0000

    Updated Server

--- server.js
+++ server.js
@@ -1,23 +1,32 @@
-// server.js
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 
-const targetUrl = 'https://wyvern.ngrok.io';
+let targetUrl = 'https://search.brave.com';
+
+app.use((req, res, next) => {
+  const newTargetUrl = req.query.target;
+  if (newTargetUrl) {
+    targetUrl = newTargetUrl;
+  }
+  next();
+});
 
 app.use(
   '/',
   createProxyMiddleware({
     target: targetUrl,
     changeOrigin: true,
-    secure: false, 
+    secure: false,
     onProxyRes(proxyRes, req, res) {
-
+      
+    },
+    onProxyReq(proxyReq, req, res) {
+      
     }
   })
 );
 
-// Start the server on port 3000
 app.listen(3000, () => {
   console.log('Proxy server is running on http://localhost:3000');
 });
